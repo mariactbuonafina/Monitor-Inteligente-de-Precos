a5c996c06f5154ede28809300e43478412e29ac9
mariactbuonafina <trevizanebuonafinamc@gmail.com>
2026-07-31 16:45:57 -0300

    remocao

--- src/api/brasilapi.py
+++
@@ -1,8 +0,0 @@
-import requests
-from config import BRASIL_API_FERIADOS
-
-
-def obter_feriados():
-    resposta = requests.get(BRASIL_API_FERIADOS, timeout=10)
-    resposta.raise_for_status()
-    return resposta.json()
